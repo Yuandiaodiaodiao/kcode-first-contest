66329bf3cd9fabc4273f4953a76042126ab1ff36
1975192920VkJ <1975192920vkj@kuaishou.com>
2020-06-26 16:34:29 +0800

    flush score

--- src/main/java/com/kuaishou/kcode/PrepareMultiThreadManager.java
+++ src/main/java/com/kuaishou/kcode/PrepareMultiThreadManager.java
@@ -16,7 +16,7 @@ public class PrepareMultiThreadManager {
     public static int RAM_CHUNCK_SIZE = MAXBUFFERLEN*3;
     public static int Time_CHUNCK_SIZE = MAXBUFFERLEN;
     public static Thread[] smbbt=new Thread[16];
-    public static int THREAD_NUMBER=5;
+    public static int THREAD_NUMBER=7;
 
     PrepareMultiThreadManager(){
         Thread prepareThread=new Thread(()->{
