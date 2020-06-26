43131d789c9286ed6d083025134674f05c31e8c8
1975192920VkJ <1975192920vkj@kuaishou.com>
2020-06-26 23:37:46 +0800

    flush score

--- src/main/java/com/kuaishou/kcode/KcodeRpcMonitorImpl.java
+++ src/main/java/com/kuaishou/kcode/KcodeRpcMonitorImpl.java
@@ -77,12 +77,12 @@ public class KcodeRpcMonitorImpl implements KcodeRpcMonitor {
         int t = 26427312 + time.charAt(9) * 1440 + time.charAt(11) * 600 + time.charAt(12) * 60 + time.charAt(14) * 10 + time.charAt(15) - SplitMinuteThread.firstTime;
 
 //        int t = 25721712 +  time.charAt(9)* 1440+ time.charAt(8) *14400+ time.charAt(11) * 600 + time.charAt(12)* 60+ time.charAt(14)* 10  + time.charAt(15)- SplitMinuteThread.firstTime;
-        if(t!=2222){
-//            throw new ArrayIndexOutOfBoundsException("prepareTime="+prepareTime);
-            throw new ArrayIndexOutOfBoundsException("T="+PrepareMultiThreadManager.THREAD_NUMBER+ " prepare="+prepareTime+" DWS="+DiskReadThread.DiskRead_waitBuffer + " SWD="+SplitMinuteThread.SplitMinute_waitBuffer + " SWM="+SplitMinuteThread.SplitMinute_waitBa);
-//            throw new ArrayIndexOutOfBoundsException("MAXSIZE="+SplitMinuteThread.MAXBUFFERLEN);
-//            throw new ArrayIndexOutOfBoundsException("MINSIZE="+SplitMinuteThread.MINBUFFERLEN);
-        }
+//        if(t!=2222){
+////            throw new ArrayIndexOutOfBoundsException("prepareTime="+prepareTime);
+//            throw new ArrayIndexOutOfBoundsException("T="+PrepareMultiThreadManager.THREAD_NUMBER+ " prepare="+prepareTime+" DWS="+DiskReadThread.DiskRead_waitBuffer + " SWD="+SplitMinuteThread.SplitMinute_waitBuffer + " SWM="+SplitMinuteThread.SplitMinute_waitBa);
+////            throw new ArrayIndexOutOfBoundsException("MAXSIZE="+SplitMinuteThread.MAXBUFFERLEN);
+////            throw new ArrayIndexOutOfBoundsException("MINSIZE="+SplitMinuteThread.MINBUFFERLEN);
+//        }
         int len1 = str1.length();
         int len2 = str2.length();
 
