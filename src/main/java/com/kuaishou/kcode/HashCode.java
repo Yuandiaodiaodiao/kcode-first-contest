d03e8d7b0dc787130cb2f309ae753301ba8e2b9a
1975192920VkJ <1975192920vkj@kuaishou.com>
2020-06-28 23:28:27 +0800

    flush score

--- src/main/java/com/kuaishou/kcode/HashCode.java
+++ src/main/java/com/kuaishou/kcode/HashCode.java
@@ -18,7 +18,7 @@ public class HashCode {
     }
 
     public static int hashIp(long ip1,long ip2){
-        return (int)((((ip1-167772160)%4985)<<4)+((ip2-167772160)%4910))%3369;
+        return (int)((((ip1-167772160)%3457)<<9)+((ip2-167772160)%2833))%2551;
     }
 
     public static int hashTwoString(String str1, String str2) {
