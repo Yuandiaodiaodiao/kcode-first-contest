e9d5d606877ddcff7fae2ae7c667c196067b83d0
1975192920VkJ <1975192920vkj@kuaishou.com>
2020-06-30 22:10:42 +0800

    flush score

---
+++ src/main/java/com/kuaishou/kcode/BufferAndCountDownPayload.java
@@ -0,0 +1,11 @@
+package com.kuaishou.kcode;
+
+import java.util.concurrent.CountDownLatch;
+
+public class BufferAndCountDownPayload {
+    byte[] buffer;
+    int startIndex;
+    int endIndex;
+    CountDownLatch countDown;
+
+}
