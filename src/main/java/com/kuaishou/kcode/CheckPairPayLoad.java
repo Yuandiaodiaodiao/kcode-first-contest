10ab068197745c3988c6d2e414322ad428fc3f2e
1975192920VkJ <1975192920vkj@kuaishou.com>
2020-06-30 22:45:54 +0800

    flush score

--- src/main/java/com/kuaishou/kcode/CheckPairPayLoad.java
+++ src/main/java/com/kuaishou/kcode/CheckPairPayLoad.java
@@ -4,7 +4,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicIntegerArray;
 
 public class CheckPairPayLoad {
-    public AtomicInteger successTimes=0;
+    public AtomicInteger successTimes=new AtomicInteger();
     public int failedTimes=0;
     public long ip;
     public AtomicIntegerArray bucket=new AtomicIntegerArray(300);
