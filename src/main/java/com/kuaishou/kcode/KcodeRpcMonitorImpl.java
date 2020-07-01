a8a989f0f9509ef38a12cf1e514712bea40ced5f
1975192920VkJ <1975192920vkj@kuaishou.com>
2020-07-01 17:58:59 +0800

    Update KcodeRpcMonitorImpl.java

--- src/main/java/com/kuaishou/kcode/KcodeRpcMonitorImpl.java
+++ src/main/java/com/kuaishou/kcode/KcodeRpcMonitorImpl.java
@@ -38,7 +38,7 @@ public class KcodeRpcMonitorImpl implements KcodeRpcMonitor {
         manager = new PrepareMultiThreadManager();
         manager.setPath(path);
         manager.start();
-        manager.stop();
+//        manager.stop();
     }
 
     public void prepare(String path) {
@@ -49,13 +49,13 @@ public class KcodeRpcMonitorImpl implements KcodeRpcMonitor {
         newPrepare(path);
         Long endTime = System.currentTimeMillis();
         prepareTime = (endTime - startTime);
-//        try {
-//            long sleeplen=3600;
-//            System.out.println("睡"+sleeplen);
-//            Thread.sleep(sleeplen);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
+        try {
+            long sleeplen=3000;
+            System.out.println("睡"+sleeplen);
+            Thread.sleep(sleeplen);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 //        System.out.println("准备时间" + prepareTime);
 //        HeatCache.HeatCheckPair();
 
@@ -119,6 +119,7 @@ public class KcodeRpcMonitorImpl implements KcodeRpcMonitor {
 
 
         } else {
+            manager.stop();
             int t = 26427312 + time.charAt(9) * 1440 + time.charAt(11) * 600 + time.charAt(12) * 60 + time.charAt(14) * 10 + time.charAt(15) - SplitMinuteThread.firstTime;
             int len1 = str1.length();
             int len2 = str2.length();
