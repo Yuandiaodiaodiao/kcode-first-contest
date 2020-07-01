e86341f7ccb6b866f481ebf11655792e5cd3738a
1975192920VkJ <1975192920vkj@kuaishou.com>
2020-07-01 19:34:34 +0800

    flush score

--- src/main/java/com/kuaishou/kcode/KcodeRpcMonitorImpl.java
+++ src/main/java/com/kuaishou/kcode/KcodeRpcMonitorImpl.java
@@ -50,7 +50,7 @@ public class KcodeRpcMonitorImpl implements KcodeRpcMonitor {
         Long endTime = System.currentTimeMillis();
         prepareTime = (endTime - startTime);
         try {
-            long sleeplen=2500;
+            long sleeplen=2700;
             System.out.println("睡"+sleeplen);
             Thread.sleep(sleeplen);
         } catch (InterruptedException e) {
